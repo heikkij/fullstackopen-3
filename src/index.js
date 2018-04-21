140f6295c98452b17a928924a9abbd6a80935370
Heikki J <heikkij@rocketmail.com>
2018-04-21 09:40:33 +0300

    3.4 puhelinluettelon backend osa 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
-const persons = [
+let persons = [
     { id: 0, name: 'Arto Hellas', number: '040 123456' },
     { id: 1, name: 'Martti Tienari', number: '040 123456' },
     { id: 2, name: 'Arto Järvinen', number: '040 123456' },
@@ -26,8 +26,14 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-  })
-  
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
 app.listen(PORT, () => {
     console.log(`Persons backend running on port ${PORT}`)
 })
